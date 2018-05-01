01bc68f1bae768192dc18a473e932165da247c79
Spidey <spideybr@gmail.com>
2018-05-01 11:34:15 -0300

    Change layout of Bicicletas.html to be dynamic
    Instead of listing all bikes in a big table, this new layout shows only
    an empty table on page load and waits for the user to click on
    individual bikes for its information to be shown.

--- pedalarte-html-site/actions.js
+++ pedalarte-html-site/actions.js
@@ -8,3 +8,17 @@ function closeWindow()
 {
 	window.close();
 }
+
+function showBike(index)
+{
+	var bikes = [
+		["MTB VOLT 1.6 PT/AM", "imagens/Bike_MTB_ARO16.jpg", "R$ 490,00"],
+		["CALOI CECI BRANCA", "imagens/Bike_Caloi_ARO16.jpg", "R$ 469,00"],
+		["POTY Branca/Vermelho", "imagens/Bike_Poty_ARO26.jpg", "R$ 489,00"],
+		["CALOI Sport T19 V21", "imagens/Bike_Caloi_ARO26.jpg", "R$ 880,00"]
+	];
+
+	document.getElementById("bikeName").innerHTML = bikes[index][0];
+	document.getElementById("bikeImage").src = bikes[index][1];
+	document.getElementById("bikePrice").innerHTML = bikes[index][2];
+}
