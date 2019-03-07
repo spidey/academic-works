eab61a049f41d7a2ce0a28d006e741d073cdbc95
Claudio Roberto França Pereira <spideybr@gmail.com>
2019-03-07 19:38:01 -0300

    Add set_union() and its tests

--- bstset/src/BalancedBSTSet.py
+++ bstset/src/BalancedBSTSet.py
@@ -209,6 +209,31 @@ def set_intersection(itr1, itr2):
             next(p2)
     return result
 
+def set_union(itr1, itr2):
+    p1 = peekable(itr1)
+    p2 = peekable(itr2)
+    result = type(itr1)()
+    while p1.hasNext() or p2.hasNext():
+        if not p1.hasNext():
+            result.update(p2)
+            break
+        if not p2.hasNext():
+            result.update(p1)
+            break
+        i1 = p1.peek()
+        i2 = p2.peek()
+        if i1 < i2:
+            result.append(i1)
+            next(p1)
+        elif i2 < i1:
+            result.append(i2)
+            next(p2)
+        else:
+            result.append(i1)
+            next(p1)
+            next(p2)
+    return result
+
 import unittest
 import random
 
@@ -398,6 +423,26 @@ class TestBalancedBSTSet(unittest.TestCase):
             if x in b:
                 c.append(x)
         self.assertEqual(c.toArray(), set_intersection(a, b).toArray())
+
+    def test_setUnion(self):
+        a = BalancedBSTSet()
+        a.update([x for x in range(1, 5)])
+        b = BalancedBSTSet()
+        b.update([x for x in range(3, 10)])
+        c = BalancedBSTSet()
+        c.update([x for x in range(1, 10)])
+        self.assertEqual(set_union(a, b).toArray(), c.toArray())
+
+        a = BalancedBSTSet()
+        dA = [random.randint(1, 500) for _ in range(random.randrange(100, 200))]
+        a.update(dA)
+        b = BalancedBSTSet()
+        dB = [random.randint(1, 500) for _ in range(random.randrange(100, 200))]
+        b.update(dB)
+        c = BalancedBSTSet()
+        c.update(dA)
+        c.update(dB)
+        self.assertEqual(c.toArray(), set_union(a, b).toArray())
 
 if __name__ == "__main__":
     unittest.main()
